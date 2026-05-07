3e9b8713fed8aabe281eb7c2da34bcc9cadcd04a
Dominic Christopher <christopherdominic268@gmail.com>
2026-05-06 20:51:05 -0500

    Creating test button for toast notification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,13 @@
 </div>
 
 <a href="/board" class="button">Play Game</a>
+
+<script lang="ts">
+    import { toast } from '@zerodevx/svelte-toast';
+
+    function showToast() {
+        toast.push("This is a toast notification!");
+    }
+</script>
+
+<button class="button" onclick = {showToast}>Test Toast</button>
